fd556cc6c2e4ea7059db6f057b8126020f55c1d4
tonhom260 <96853822+tonhom260@users.noreply.github.com>
2022-04-06 13:24:42 +0700

    update

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -28,6 +28,10 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         color: index.isEven?Color(0xFF69c5df):Color(0xFF9294cc),
+        image: DecorationImage(
+          fit:BoxFit.cover ,
+          image: AssetImage("assets/image/food$index.png")
+        )
       ),
 
 
