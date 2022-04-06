acd1c26d9b53f70f1187934d0af8bfca184a9302
ton <tonhom260@gmail.com>
2022-04-06 07:38:36 +0700

    initial MyApplication Proj

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../utils/colors.dart';
+
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
 
@@ -11,29 +13,40 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        child: Row(
-          children: [
-            Column(
-              children: [
-                Text('Country'),
-                Text('City'),
-                Text('DishEmpty')
+    return Scaffold(body: Column(
+      children: [
+         Container(
 
+          child: Container(
+            margin: EdgeInsets.only(top: 45,bottom: 15,),
+            padding: EdgeInsets.only(left: 20,right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: const [
+                    Text('Country'),
+                    Text('City'),
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    child: Icon(Icons.search,color: Colors.white,),
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: AppColors.mainColor,
+                    ),
+                  ),
+                )
               ],
             ),
-            Container(
-              width:45,
-              height:45,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.blue,
-              ),
-            )
-          ],
+          ),
         ),
-      ),
+      ],
+    ),
+
     );
   }
 }
