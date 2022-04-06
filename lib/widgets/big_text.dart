1a7bf1a997f5dd2503313ed74c668e01967c33c1
ton <tonhom260@gmail.com>
2022-04-06 08:03:24 +0700

    initial MyApplication Proj

---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+class Bigtext extends StatelessWidget {
+  final Color? color;
+  final String text;
+  final double size;
+  final TextOverflow overFlow;
+  const Bigtext({Key? key, this.color,
+    required this.text,
+    this.size = 20.0,
+    this.overFlow=TextOverflow.ellipsis})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overFlow,
+      style: TextStyle(fontSize: size,color: color,fontFamily: 'Roboto',
+          fontWeight: FontWeight.w400),
+
+
+    );
+  }
+}
