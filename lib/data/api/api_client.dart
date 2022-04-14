81cdbeed629fc67bced42ef8f023a9732c2a3427
ton <tonhom260@gmail.com>
2022-04-14 10:51:03 +0700

    update

--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -2,11 +2,16 @@ import 'package:get/get.dart';
 
 class ApiClient extends GetConnect implements GetxService{
   late String token;
+  late Map <String,String> _mainHeaders;
   final String appBaseUrl;
 
   ApiClient({required this.appBaseUrl}){
     baseUrl = appBaseUrl;//baseUrl is from GET PACKAGE
-    timeout = Duration (seconds: 30);
+    timeout = const Duration (seconds: 30);
+    _mainHeaders={
+      'Content-type' : 'application/json; charset=UTF-8',
+      'Authorization' : 'Bearer $token',
+    };
   }
 
 
